fad0788d72d268a6825e8763066fa8bcc5bce7d0
DESKTOP-V6CKKJH\Nikola Savic <nikolasavic@outlook.com>
2018-05-04 12:34:49 +0200

    Wordpress Cheatsheet

---
+++ wordpress-cheatsheet.php
@@ -0,0 +1,8 @@
+<?php 
+    Adding [wp_enque_style] into functions.php of a theme to link Material Icons externaly.
+    wp_enqueue_style ('material-icons', '//fonts.googleapis.com/icon?family=Material+Icons'); 
+    // <ul>
+ 	// <li><i class="material-icons">phone_android</i><a href="mailto:some@mail.com" target="_top">some@mail.com</a></li>
+ 	// <li><i class="material-icons">phone_android</i><a href="tel:+387 66 268 639" target="_top">tel:+387 66 268 639</a></li>
+</ul>
+?>
