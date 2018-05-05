aa383b62888f508ac9ab244bd8980ebeeb69bc43
DESKTOP-V6CKKJH\Nikola Savic <nikolasavic@outlook.com>
2018-05-05 20:45:01 +0200

    wp_enque_script

--- wordpress-cheatsheet.php
+++ wordpress-cheatsheet.php
@@ -1,8 +1,15 @@
 <?php 
+
+    // Script 
+
     Adding [wp_enque_style] into functions.php of a theme to link Material Icons externaly.
     wp_enqueue_style ('material-icons', '//fonts.googleapis.com/icon?family=Material+Icons'); 
     // <ul>
  	// <li><i class="material-icons">phone_android</i><a href="mailto:some@mail.com" target="_top">some@mail.com</a></li>
  	// <li><i class="material-icons">phone_android</i><a href="tel:+387 66 268 639" target="_top">tel:+387 66 268 639</a></li>
-</ul>
+    // </ul>
+
+    // Style
+
+     wp_enqueue_script ('ime-skripte', 'neka-skripta');
 ?>
