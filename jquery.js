bbbf2ac28361174794a905506fef869ff0f63adf
DESKTOP-V6CKKJH\Nikola Savic <nikolasavic@outlook.com>
2018-05-10 19:53:28 +0200

    Addings

--- jquery.js
+++ jquery.js
@@ -5,9 +5,18 @@
 
 ///////////////////////////////////////////
 
+// Targeting elements by type: by class: by id:
 $(document).ready(function () {
-    $("button").addClass("animated bounce")
-    $("button") // element to .addClass to // animated and bounce options are included before
-    $(".some-class") // adding to a class 
-    $("#some-id") // adding to an ID
+
+    // Targeting elements by type: by class: by id:
+    $("button").addClass("animated bounce");
+    $("button"); // element to .addClass to // animated and bounce options are included before
+    $(".some-class"); // adding to a class 
+    $("#some-id"); // adding to an ID
+
+    // Removing classes
+    $("button").removeClass("some classes");
+
+    // Changing of CSS
+    $("button").css("color", "orange");
 }    
---
+++ wordpress-cheatsheet.js
@@ -0,0 +1,44 @@
+    // Script 
+
+    Adding [wp_enque_style] into functions.php of a theme to link Material Icons externaly.
+    wp_enqueue_style ('material-icons', '//fonts.googleapis.com/icon?family=Material+Icons'); 
+    // <ul>
+ 	// <li><i class="material-icons">phone_android</i><a href="mailto:some@mail.com" target="_top">some@mail.com</a></li>
+ 	// <li><i class="material-icons">phone_android</i><a href="tel:+387 66 268 639" target="_top">tel:+387 66 268 639</a></li>
+    // </ul>
+
+    // Style
+
+     wp_enqueue_script ('ime-skripte', 'neka-skripta');
+
+     // Full codex here 
+     // https://developer.wordpress.org/reference/functions/wp_enqueue_script/#Default_scripts_included_with_WordPress
+     // Dev Guide Conflict free CSS and JavaScript
+     // https://www.smashingmagazine.com/2011/10/developers-guide-conflict-free-javascript-css-wordpress/
+
+
+     // Wordpress uses 90% of jpeg quality for images
+     // Fix
+     add_filter( 'jpeg_quality', 'smashing_jpeg_quality' );
+    function smashing_jpeg_quality() {
+        return 100;
+
+
+    // Wordpress external Jquery and other scripts 
+    https://wordpress.org/plugins/use-google-libraries/
+
+
+    ////////////////////////////////////////////////////////////////////
+    //////////////////////////// jQuery ////////////////////////////////
+    ////////////////////////////////////////////////////////////////////
+
+
+    // Changing any text
+    jQuery(document).ready(function () {
+    jQuery(".nesto1").html("O Nama");
+    });
+
+    // Adding Css
+    jQuery(document).ready(function () {
+    jQuery(".nesto1").css("color", "red");
+    });
