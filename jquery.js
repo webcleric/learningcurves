8ceae78dba929909090ac8691b5e861804470b2d
DESKTOP-V6CKKJH\Nikola Savic <nikolasavic@outlook.com>
2018-05-10 10:49:04 +0200

    jQuery

---
+++ jquery.js
@@ -0,0 +1,13 @@
+
+///////////////////////////////////////////
+
+// jQuery
+
+///////////////////////////////////////////
+
+$(document).ready(function () {
+    $("button").addClass("animated bounce")
+    $("button") // element to .addClass to // animated and bounce options are included before
+    $(".some-class") // adding to a class 
+    $("#some-id") // adding to an ID
+}    
